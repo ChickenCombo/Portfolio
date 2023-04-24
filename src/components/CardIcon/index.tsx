a948375a81acdd619a6075987ec1812a44f0156f
Lester Cuadra <lester.cuadra24@gmail.com>
2023-04-24 14:10:30 +0800

    feat: popover

--- src/components/CardIcon/index.tsx
+++ src/components/CardIcon/index.tsx
@@ -1,17 +1,51 @@
-import { ReactElement } from "react";
-import { Container, Card } from "./styles";
+import { ReactElement, useState } from "react";
+import { Container, Card, Popover } from "./styles";
+import { P } from "../global";
+
+interface CardIcon {
+  name: string;
+  icon: ReactElement;
+}
 
 interface CardIconProps {
-  icons: ReactElement[];
+  icons: CardIcon[];
 }
 
 const CardIcon = (props: CardIconProps) => {
   const { icons } = props;
 
+  const [isPopoverVisible, setIsPopOverVisible] = useState(false);
+  const [hoveredIndex, setHoveredIndex] = useState(-1);
+
+  const onMouseEnter = (index: number) => {
+    setIsPopOverVisible(true);
+    setHoveredIndex(index);
+  };
+
+  const onMouseLeave = () => {
+    setIsPopOverVisible(false);
+    setHoveredIndex(-1);
+  };
+
   return (
     <Container>
-      {icons.map((icon) => (
-        <Card>{icon}</Card>
+      {icons.map((icon, index) => (
+        <>
+          <Card
+            key={index}
+            onMouseEnter={() => onMouseEnter(index)}
+            onMouseLeave={onMouseLeave}
+          >
+            {icon.icon}
+            <Popover
+              className={`${
+                isPopoverVisible && hoveredIndex === index ? "" : "hidden"
+              }`}
+            >
+              <P>{icon.name}</P>
+            </Popover>
+          </Card>
+        </>
       ))}
     </Container>
   );
--- src/components/CardIcon/styles.tsx
+++ src/components/CardIcon/styles.tsx
@@ -1,12 +1,15 @@
 import tw from "tailwind-styled-components";
 
 export const Container = tw.div`
-    flex flex-row flex-wrap w-full justify-center
+  flex flex-row flex-wrap w-full justify-center
 `;
 
 export const Card = tw.button`
-    h-16 w-16 grid place-items-center rounded-md border pointer bg-white m-1
-    hover:border-gray-300
-    transition-colors
-    text-4xl
+  h-16 w-16 grid place-items-center rounded-md border pointer bg-white m-1 transition-colors text-4xl
+  hover:border-gray-300
+`;
+
+export const Popover = tw.div`
+  absolute h-10 min-w-20 bg-white shadow-sm rounded-lg p-4 text-sm border transform translate-y-9
+  flex justify-center items-center
 `;
